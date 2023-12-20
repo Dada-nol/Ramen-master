bcf9fd22aa2daf38477024bf1a2d6efc9f91864d
Darren <nardol.darren@gmail.com>
2023-12-20 16:51:28 +0100

    add html

--- Ramen master/script.js
+++ Ramen master/script.js
@@ -1,4 +1,3 @@
- // JavaScript pour gérer la soumission du formulaire de réservation
  const reservationForm = document.getElementById('reservationForm');
  reservationForm.addEventListener('submit', (event) => {
      event.preventDefault();
@@ -12,11 +11,5 @@
           body: JSON.stringify({ name, date, time }),
           headers: { 'Content-Type': 'application/json' },
       })
-      .then(response => response.json())
-      .then(data => {
-          alert('Réservation confirmée !');
-      })
-      .catch(error => {
-          alert("Une erreur s'est produite lors de la réservation.")
-      });
+      
  });
